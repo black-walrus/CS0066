b89c84ea2cf90823d7a834756d0a392a6f26a89d
sauljacobson <gruspeseth@gmail.com>
2024-09-23 14:24:24 +0800

    renamed parameter names and added comments

--- IntroJavascript/BeginnerEVM/PokeTokenMinter.sol
+++ IntroJavascript/BeginnerEVM/PokeTokenMinter.sol
@@ -1,26 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27; 
 
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
 
 contract PokeTokenMinter {
+
+    // public variables here
     string public tokenName = "PokeToken"; 
-    string public tokenAbbrev = "PokeT";
+    string public tokenAbbrev = "PKT";
     uint public totalSupply = 0; 
 
+    // mapping variable here
     mapping(address => uint) balances; 
 
-    function mintNFT(address sender, uint amount) public {
-        totalSupply += amount; 
-        balances[sender] += amount;
+    // mint function
+    function mintNFT(address addr, uint value) public {
+        totalSupply += value; 
+        balances[addr] += value;
     }
 
-    function burnNFT(address sender, uint amount) public {
-        if (balances[sender] < amount) {
-            return;
+    // burn function
+    function burnNFT(address addr, uint value) public {
+        if (balances[addr] >= value) {
+            balances[addr] -= value;
+            totalSupply -= value; 
         }
-
-        balances[sender] -= amount;
-        totalSupply -= amount; 
     }
-
 }
