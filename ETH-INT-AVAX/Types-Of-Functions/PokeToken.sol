cbbc322cf765b1d55e4e634e3f45c3bb7b4aa3c7
sauljacobson <gruspeseth@gmail.com>
2024-09-25 19:49:07 +0800

    fixed error handling

--- ETH-INT-AVAX/Types-Of-Functions/PokeToken.sol
+++ ETH-INT-AVAX/Types-Of-Functions/PokeToken.sol
@@ -19,14 +19,15 @@ contract PokeToken is ERC20 {
 
     function transfer(address to, uint amount) public override returns (bool) {
         require(amount > 0, "Transfer amount must be more than 0 PKT"); 
-        require(balanceOf(msg.sender) <= amount, "Insufficient PKT to transfer");
+        require(balanceOf(msg.sender) >= amount, "Insufficient PKT to transfer");
         _transfer(msg.sender, to, amount);
         return true;
     }
 
     function burn(address from, uint amount) public {
         require(amount > 0, "Burn amount must be more than 0 PKT"); 
-        require(balanceOf(from) <= amount, "Insufficient amount of PKT to burn");
+        require(balanceOf(from) >= amount, "Insufficient amount of PKT to burn");
         _burn(from, amount);
     }
 }
+
